5735a580d58038c13342d4871978e599a7e5c0b5
Edufig85 <59071737+Edufig85@users.noreply.github.com>
2026-04-28 16:25:31 -0300

    Create page.jsx

---
+++ frontend/app/admin/painel/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function PainelAdmin() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Painel Admin</h1>
+      <p>Página carregada com sucesso.</p>
+    </div>
+  );
+}
