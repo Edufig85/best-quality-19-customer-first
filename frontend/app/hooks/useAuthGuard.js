e1f0d225c6b28059a12f0694ecf7a056e950982c
Edufig85 <59071737+Edufig85@users.noreply.github.com>
2026-04-28 15:24:54 -0300

    Update useAuthGuard.js

--- frontend/app/hooks/useAuthGuard.js
+++ frontend/app/hooks/useAuthGuard.js
@@ -1,16 +1,12 @@
 "use client";
-
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function useAuthGuard() {
   const router = useRouter();
-
   useEffect(() => {
-    const logged = localStorage.getItem("logged");
-    if (!logged) {
+    if (!localStorage.getItem("logged")) {
       router.push("/login");
     }
   }, [router]);
 }
-``
