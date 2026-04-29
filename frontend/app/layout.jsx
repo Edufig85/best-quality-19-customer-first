e0d5b86e0099b5ff290a38cdde48feebcc91a531
Edufig85 <59071737+Edufig85@users.noreply.github.com>
2026-04-29 09:34:11 -0300

    Create layout.jsx

---
+++ frontend/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "BQ19",
+  description: "Sistema BQ19",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
