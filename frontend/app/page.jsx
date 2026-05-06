a83aa586ca2f1d3e92a63db468444f1e37972856
Edufig85 <59071737+Edufig85@users.noreply.github.com>
2026-05-06 15:02:06 -0300

    Create page.jsx

---
+++ frontend/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Best Quality 19 – Customer First</h1>
+      <p>Sistema administrativo.</p>
+    </main>
+  );
+}
