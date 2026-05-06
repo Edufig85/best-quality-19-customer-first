b5e6cf78d0480838d7fdce807ffa5a53235d5bac
Edufig85 <59071737+Edufig85@users.noreply.github.com>
2026-05-06 14:58:10 -0300

    Create layout.jsx

---
+++ frontend/app/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Best Quality 19 – Customer First",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
