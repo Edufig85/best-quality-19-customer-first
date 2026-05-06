e2273f305a8a5f39e12fc61ed923ee39c0d1a552
Edufig85 <59071737+Edufig85@users.noreply.github.com>
2026-05-06 15:58:16 -0300

    Update page.jsx

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>Best Quality 19 – Customer First</h1>
+    <main style={{ padding: 40 }}>
+      <h1>Best Quality 19</h1>
       <p>Sistema administrativo.</p>
     </main>
   );
